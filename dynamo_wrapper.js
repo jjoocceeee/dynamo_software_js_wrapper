9dcfc4397aeb2ce9fd62f4e5dcbda48ab20a9160
JoCee Porter <JoCee.Porter@imail.org>
2018-11-16 13:08:13 -0700

    Cleaning up the logging code.

--- dynamo_wrapper.js
+++ dynamo_wrapper.js
@@ -19,17 +19,14 @@ var options = {
 
 
 
-/*
-  Sends a request based on options
-  Do not change.
-*/
-async function send_request(){
-    request(options, (error, response, body)=>{
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body); // Print the HTML for the page
-  });
-};
+// /*
+//   Sends a request based on options
+//   Do not change.
+// */
+// async function send_request(){
+//     request(options, (error, response, body)=>{
+//   });
+// };
 
 
 
@@ -41,7 +38,6 @@ async function send_request(){
                                           e.g. DOC, pdf, msg
 */
 async function create_entity(entityType, body) {
-  // console.log("Promise");
   var sql_connection = new Promise(async(resolve, reject)=>{
     options.url =  `https://apiuat.dynamosoftware.com/api/v2.0/entity/${entityType}`
     options.body = body;  
@@ -52,8 +48,7 @@ async function create_entity(entityType, body) {
         }
         else {
           resolve({response, body});
-          // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          //console.log('body:', body); // Print the HTML for the page
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         }
       });
   });
@@ -80,8 +75,6 @@ async function get_create_entity(entityType) {
         }
         else {
           resolve({response, body});
-          // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          //console.log('body:', body); // Print the HTML for the page
         }
       });
   });
@@ -99,29 +92,20 @@ async function get_create_entity(entityType) {
 */
 async function upload_document(title, extension, content){
   body = `{"Title":"${title}","Extension":".${extension}","_content":"${content}"}`;
-  console.log("awaiting reaponse");
-  response = await get_create_entity('Document', body);
-  // console.log(response);
+  response = await create_entity('Document', body);
   return response;
 }
-//TODOO:
-//"Documentdate": "2018-09-07T16:55:12"
-//"Funds": "",
-//Get Fund ID name so I can create a relation.
-
 
 
 /*
   Gets an array of all funds.
+  Internal Helper Function. Do not touch.
 */
 async function get_entity_ids(entity){
   var entities = new Promise(async(resolve, reject)=>{
     response = await get_create_entity(entity);
     let j = JSON.parse(response.body);
     let data = j.data;
-    // for(let i = 0; i< 10; i++){
-    //   console.log(data[i].Identifier);
-    // }
     if(data != undefined){  
     resolve(data);
     }
@@ -140,11 +124,8 @@ async function get_fund_id(fund_name){
   let fund_info = new Promise(async(resolve, reject)=>{
     funds = await get_entity_ids('Fund');
     var count = 0;
-    console.log(fund_name);
     for (let i = 0; i<funds.length; i++) {
-      // console.log(funds[i].Identifier);
       if(funds[i].Identifier == fund_name){
-        console.log("Funds match!!");
         resolve(funds[i]._id);
       }
     }
@@ -154,18 +135,26 @@ async function get_fund_id(fund_name){
 
 /*
   Gets all the available document tags.
-                  Parameter: fund_name      => Name of fund
+                  Parameter: tag_name           => type of id you are looking for. 
+                             relationship_table => Which table to look for the relationship
+                                                   e.g. L_Document_categories
 */
-async function get_Doc_tag(tag_name){
+async function get_relation(tag_name, relationship_table){
   var doc_tag = new Promise(async(resolve, reject)=>{
-    tags = await get_entity_ids('L_Document_categories');
+    tags = await get_entity_ids(relationship_table);
     // console.log(tags);
     var count = 0;
     for (let i = 0; i<tags.length; i++) {
-      console.log(tags[i].LookupName);
+      count++;
       if(tags[i].LookupName == tag_name){
         resolve(tags[i]._id);
+        break;
       }
+    }
+    if(count == tags.length){
+      log_text = `Updating document tage from ${tag_name} to Misc`
+      //Insert Misc _ID and resolve the id.
+      reject("Couldn't find ", tag_name);
     }
   });
   return doc_tag;
@@ -184,7 +173,6 @@ async function add_manager({last_name = "", first_name = "", title = "", company
 {
   body = `{"LastName":"${last_name}","FirstName":"${first_name}","Jobtitle":"${title}","Company":"${company}","ContactInfo_Email":"${email}","ContactInfo_BusinessAddress_Street":"${street_line}","ContactInfo_HomeAddress_City":"${city}","ContactInfo_BusinessAddress_State":"${state}","ContactInfo_BusinessAddress_Country":"${country}","ContactInfo_BusinessAddress_Zip":"${zip_code}","ContactInfo_BusinessPhone":"${phone}"}`;
   response = await create_entity('Contact', body);
-  console.log("RETURNING");
   return response.body.data._id;
 }
 
@@ -197,10 +185,10 @@ async function add_manager({last_name = "", first_name = "", title = "", company
 */
 module.exports.upload_document = upload_document;
 module.exports.create_entity = create_entity;
-module.exports.send_request = send_request;
+// module.exports.send_request = send_request;
 module.exports.add_manager = add_manager;
 module.exports.get_fund_id = get_fund_id;
-module.exports.get_Doc_tag = get_Doc_tag;
+module.exports.get_relation = get_relation;
 
 
 
